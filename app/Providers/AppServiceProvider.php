11e3381405bfa8ec149671c1b73a82f17243ec7c
wrlckstr <wrlckstr@outlook.jp>
2023-09-10 19:54:30 +0900

    chenge https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,5 +21,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
+
+        if(\App::environment(['production'])){
+            \URL::forseScheme('https');
+        }
     }
 }
