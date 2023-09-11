74b6d29e5cf90e51aeda665a4473c4f95cb11d83
wrlckstr <wrlckstr@outlook.jp>
2023-09-11 19:44:28 +0900

    2023.09.11.5develop

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -78,7 +78,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'role'=>'10',
+            'role'=>'100',
         ]);
     }
 }
